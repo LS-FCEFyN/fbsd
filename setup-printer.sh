15ad6bcec63f3a67ff5862b616993853b547951d
защитников Москвы <eljefemaestro3087@hotmail.com>
2021-02-23 11:24:03 -0300

    Update setup-printer.sh

--- setup-printer.sh
+++ setup-printer.sh
@@ -16,7 +16,7 @@ else
 	add path 'unlpt*' mode 0660 group cups
 	add path 'ulpt*' mode 0660 group cups
 	add path 'lpt*' mode 0660 group cups
-	add path 'usb/X.Y.Z' mode 0660 group cups 
+	add path 'usb/0.2' mode 0660 group cups 
 EOT
 fi
 
