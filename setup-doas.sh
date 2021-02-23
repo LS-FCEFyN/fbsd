96c9cf81fef280bc23bd0aea33ea821ba67a852b
защитников Москвы <eljefemaestro3087@hotmail.com>
2021-02-23 11:12:19 -0300

    Add files via upload

---
+++ setup-doas.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+touch /usr/local/etc/doas.conf 
+
+cat <<EOT >> /usr/local/etc/doas.conf
+# Configuration file for doas
+# Please see doas.conf manual page for information on setting
+# up a doas.conf file.
+
+# Permit user lore to run programs as root, maintaining
+# envrionment variables. Useful for GUI applications.
+permit keepenv lore as root
+EOT 
---
+++ setup-printer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+FILE=/etc/devfs.rules
+if test -f "$FILE"; then
+	cat <<EOT >> $FILE 
+	[system=10]
+	add path 'unlpt*' mode 0660 group cups
+	add path 'ulpt*' mode 0660 group cups
+	add path 'lpt*' mode 0660 group cups
+	add path 'usb/0.2' mode 0660 group cups 
+EOT
+else
+	touch $FILE
+	cat <<EOT >> $FILE 
+	[system=10]
+	add path 'unlpt*' mode 0660 group cups
+	add path 'ulpt*' mode 0660 group cups
+	add path 'lpt*' mode 0660 group cups
+	add path 'usb/X.Y.Z' mode 0660 group cups 
+EOT
+fi
+
+echo "devfs_system_ruleset=\"system\"" >> /etc/rc.conf
+
+/etc/rc.d/devfs restart
+/usr/local/etc/rc.d/cupsd restart
