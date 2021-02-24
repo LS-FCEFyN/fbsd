f1800037baf619fed818fac4304f8e798e2548b2
защитников Москвы <eljefemaestro3087@hotmail.com>
2021-02-24 01:46:55 -0300

    Update setup-printer.sh

--- setup-printer.sh
+++ setup-printer.sh
@@ -11,7 +11,7 @@ if test -f "$FILE"; then
 	add path 'unlpt*' mode 0660 group cups
 	add path 'ulpt*' mode 0660 group cups
 	add path 'lpt*' mode 0660 group cups
-	add path 'usb/0.2' mode 0660 group cups 
+	add path 'usb/X.Y.Z' mode 0660 group cups 
 EOT
 else
 	touch $FILE
@@ -20,7 +20,7 @@ else
 	add path 'unlpt*' mode 0660 group cups
 	add path 'ulpt*' mode 0660 group cups
 	add path 'lpt*' mode 0660 group cups
-	add path 'usb/0.2' mode 0660 group cups 
+	add path 'usb/X.Y.Z' mode 0660 group cups 
 EOT
 fi
 
