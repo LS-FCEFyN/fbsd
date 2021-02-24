f5a9fb24630558c03b77c527ab571bb1cef82557
защитников Москвы <eljefemaestro3087@hotmail.com>
2021-02-24 03:34:07 -0300

    Update setup-doas.sh

--- setup-doas.sh
+++ setup-doas.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-touch /usr/local/etc/doas.conf 
+DEFAULT_USER=`grep 1001 /etc/passwd | awk -F: '{ print $1 }'`
+
+touch /usr/local/etc/doas.conf
 
 cat <<EOT >> /usr/local/etc/doas.conf
 # Configuration file for doas
 # Please see doas.conf manual page for information on setting
 # up a doas.conf file.
 
-# Permit user lore to run programs as root, maintaining
+# Permit $DEFAULT_USER to run programs as root, maintaining
 # envrionment variables. Useful for GUI applications.
-permit keepenv lore as root
+permit keepenv $DEFAULT_USER as root
 EOT
